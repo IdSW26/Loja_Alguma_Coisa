458e78293a8e5845f0833defb5f1988efe136503
Isaque_Witt <167255023+IdSW26@users.noreply.github.com>
2026-04-15 00:15:18 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+function avisaCadastro(){
+    alert("Cadastro realizado com sucesso!");
+}
+function avisaLogin(){
+    alert("Login realizado com sucesso!");
+}
+function avisaCompra(){
+    alert("Compra realizada com sucesso!");
+}  
